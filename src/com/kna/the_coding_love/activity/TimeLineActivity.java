7a4c9fb39c67553a8b6d80ed617d6346b4054fd0
Pablo Cañamares Ullan <pablo.canamares@intelygenz.com>
2013-12-24 11:14:08 +0100

    Change view

--- src/com/kna/the_coding_love/activity/TimeLineActivity.java
+++ src/com/kna/the_coding_love/activity/TimeLineActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.kna.the_coding_love.R;
@@ -13,22 +16,29 @@ import com.kna.the_coding_love.interfaze.OnResponseGetGifPost;
 import com.kna.the_coding_love.model.GifPost;
 import com.kna.the_coding_love.net.NetManager;
 
-public class TimeLineActivity extends Activity implements OnResponseGetGifPost{
+public class TimeLineActivity extends Activity implements OnResponseGetGifPost {
 
 	private String LOG_TAG = "----->TimeLineActivity";
-	
+
 	private TextView textView;
-	
+
+	private LinearLayout linearLayoutScroll;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_time_line);
 
 		textView = (TextView) findViewById(R.id.textViewTimeLine);
-		
-		//Make request
+		linearLayoutScroll = (LinearLayout) findViewById(R.id.linearLayoutScroll);
+
+		// Make request
 		NetManager.getInstance(this).getGifPost(this);
-		
+
+		for (int i = 0; i < 5; i++) {
+			RelativeLayout viewGifPost = (RelativeLayout) getLayoutInflater().inflate(R.layout.gif_post, null);
+			linearLayoutScroll.addView(viewGifPost);
+		}
 	}
 
 	@Override
@@ -38,13 +48,7 @@ public class TimeLineActivity extends Activity implements OnResponseGetGifPost{
 		return true;
 	}
 
-	
-	
-	
-	
-	
-	
-	//Callbacks
+	// Callbacks
 	@Override
 	public void onResponseGetGifPost(ArrayList<GifPost> gifPosts) {
 		Log.d(LOG_TAG, "onResponseGetGifPost gifPosts size: " + gifPosts.size());
@@ -57,7 +61,6 @@ public class TimeLineActivity extends Activity implements OnResponseGetGifPost{
 
 	@Override
 	public void onResponseGetGifPostRaw(String response) {
-		textView.setText(response);
 		Log.d(LOG_TAG, "onResponseGetGifPost response");
 	}
 
